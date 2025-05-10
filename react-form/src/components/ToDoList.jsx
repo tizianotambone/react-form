1d036fa983f895a170633459b7341f9ef374e47a
tizianotambone <tambonetiziano@gmail.com>
2025-05-10 15:49:38 +0200

    [UPD] addArticle

--- react-form/src/components/ToDoList.jsx
+++ react-form/src/components/ToDoList.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
+
+const articles = [
+    "Come migliorare la produttività lavorando da casa",
+    "Guida introduttiva a JavaScript per principianti",
+    "I 10 libri da leggere assolutamente nel 2025",
+    "Strategie per risparmiare sulle spese mensili",
+    "Le migliori destinazioni per viaggiare in primavera",
+    "Cos'è l'intelligenza artificiale e come sta cambiando il mondo",
+    "Alimentazione sana: 5 consigli pratici",
+    "Come creare un sito web con HTML e CSS",
+    "La storia del cinema italiano in 10 film",
+    "Esercizi semplici per mantenersi in forma a casa"
+];
 
 function ToDoList() {
-    const articoli = [
-        "Come migliorare la produttività lavorando da casa",
-        "Guida introduttiva a JavaScript per principianti",
-        "I 10 libri da leggere assolutamente nel 2025",
-        "Strategie per risparmiare sulle spese mensili",
-        "Le migliori destinazioni per viaggiare in primavera",
-        "Cos'è l'intelligenza artificiale e come sta cambiando il mondo",
-        "Alimentazione sana: 5 consigli pratici",
-        "Come creare un sito web con HTML e CSS",
-        "La storia del cinema italiano in 10 film",
-        "Esercizi semplici per mantenersi in forma a casa"
-    ];
+    const [articolo, setArticolo] = useState("");
+    const [articoli, setArticles] = useState(articles);
+
+    const addArticle = (e) => {
+        e.preventDefault();
+        setArticles([...articoli, articolo]);
+        setArticolo("");
+    }
 
   return (
-    articoli.map((articolo, index) => (
-        <ul>
-            <li key={index}>{articolo}</li>
-        </ul>
-    ))
+    <>
+    {
+        articoli.map((articolo, index) => (
+            <ul>
+                <li key={index}>{articolo}</li>
+            </ul>
+        ))
+    }
+    <form onSubmit={addArticle}>
+        <hr/>
+        <label htmlFor="articolo">Aggiungi un articolo:</label>
+        <input type="text" id="articolo" name="articolo" value={articolo} onChange={(e)=>{setArticolo(e.target.value)}} placeholder='nuovo articolo'/>
+        <button type="submit">Aggiungi</button>
+    </form>
+    </>
   )
 }
 

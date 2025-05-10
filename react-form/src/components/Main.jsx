b32add16bd1e94e2c32f9fb1e24743a7c12fa48c
tizianotambone <tambonetiziano@gmail.com>
2025-05-10 15:33:32 +0200

    [UPD] main

---
+++ react-form/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import ToDoList from './ToDoList'
+
+function Main() {
+  return (
+    <>
+      <ToDoList></ToDoList>
+    </>
+  )
+}
+
+export default Main
